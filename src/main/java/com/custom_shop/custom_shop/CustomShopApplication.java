abffcb35d549ef4e4cc086d4bddfd27015de7926
Mario Adrian Turk <ma_turk@yahoo.com>
2022-12-09 16:24:39 -0300

    first commit since rework

---
+++ src/main/java/com/custom_shop/custom_shop/CustomShopApplication.java
@@ -0,0 +1,13 @@
+package com.custom_shop.custom_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custom_shop/custom_shop/CustomShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom_shop.custom_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
